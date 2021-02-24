ee4927ea4eb7f1f6a5d528309bb8b81b4e1414b5
Germán Gasset Martí <76014247+GGasset@users.noreply.github.com>
2021-02-24 23:57:41 +0100

    Add files via upload

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,30 @@
+namespace DataStructures
+{
+    public class BinaryTree<T>
+    {
+        public TreeNode Root;
+
+        public void Add(T value)
+        {
+            if (Root == null)
+                Root = new TreeNode(value);
+            else
+            {
+                
+            }
+        }
+
+
+        class TreeNode
+        {
+            public TreeNode(T value)
+            {
+                parent = leftChild = rightChild = null;
+                this.value = value;
+            }
+
+            public TreeNode parent, leftChild, rightChild;
+            public T value;
+        }
+    }
+}

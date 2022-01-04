ea4d6916e7d2e85b3e9c51d5fdde761501e75207
Germán Gasset <gassetgerman@gmail.com>
2022-01-04 20:43:24 +0100

    Added Game abstract class data structure

---
+++ Others/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public abstract class Game
+    {
+        public abstract void SetUp();
+
+        public abstract void Update();
+    }
+}
